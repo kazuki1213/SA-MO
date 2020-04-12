3846e28fb2747a6b890f9aa93b3574fe87506a9a
kanuu <123kanuu@gmail.com>
2020-04-12 14:33:04 +0900

    ユーザーコントローラーコミット漏れ

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(update_params)
+      flash[:success] = 'ユーザー情報を編集しました。'
+      redirect_to :edit
+    else
+      flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
+      render :edit
+    end
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit(:gender, :age, :height, :weight)
+  end
+end
