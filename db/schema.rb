2f2af997e28a682208ed1487aef60e71d29b7330
kanuu <kanuu@users.noreply.github.com>
2020-04-12 14:28:35 +0900

    User profile (#15)
    * 配置の変更
    
    * ユーザープロフィール画面を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_032539) do
+ActiveRecord::Schema.define(version: 2020_04_12_044259) do
 
   create_table "body_temperatures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id"
@@ -42,6 +42,10 @@ ActiveRecord::Schema.define(version: 2020_04_12_032539) do
     t.string "uid"
     t.string "provider"
     t.string "image"
+    t.integer "gender"
+    t.integer "age"
+    t.integer "height"
+    t.integer "weight"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
