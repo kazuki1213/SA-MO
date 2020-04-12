5ceb937fb63ee65ecb3e6a196ce90369cdc8a5e5
Kazuki Nozawa <kazuki1213@gmail.com>
2020-04-12 16:02:21 +0900

    add body temperature new function

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
+  has_many :body_temperatures, dependent: :destroy
 
   enum gender: [:male, :female, :other]
-  
+
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
 
